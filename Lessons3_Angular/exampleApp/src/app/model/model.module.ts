326bd8dba907b1443d54fa8c9e10b699b648c602
ngortsuev <n.gortsuev@outlook.com>
2019-05-12 21:05:20 +0300

    change project (24 2)

--- Lessons3_Angular/exampleApp/src/app/model/model.module.ts
+++ Lessons3_Angular/exampleApp/src/app/model/model.module.ts
@@ -1,8 +1,10 @@
 import { NgModule } from "@angular/core";
 import { StaticDataSource } from "./datasource.model";
 import { Model } from "./repository.model";
+import { HttpModule } from "@angular/http";
 
 @NgModule({
+    imports: [HttpModule],
     providers: [Model, StaticDataSource]
 })
 export class ModelModule { }
